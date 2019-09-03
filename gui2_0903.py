5848133f81d5c7916afaf0d77e016e3ee988f306
Elwing-Chou <birdfan8814@gmail.com>
2019-09-03 13:25:44 +0800

    Add files via upload

--- gui2_0903.py
+++ gui2_0903.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from jieba.analyse import extract_tags
 
 class MyFrame(Frame):
     def __init__(self, master):
@@ -9,19 +10,24 @@ class MyFrame(Frame):
         self.t1.pack()
         self.b1 = Button(self,
                          text="分析",
-                         command=self.analyse)
-        self.b1.pack()
+                         command=self.analyse,
+                         bg="orange")
+        self.b1.pack(pady=20,
+                     expand=True,
+                     fill=BOTH)
         self.result = Label(self, text="按上面得到關鍵詞")
         self.result.pack()
 
     def analyse(self):
-        self.result["text"] = "!!!!"
+        news = self.t1.get("1.0", "end")
+        keywords = extract_tags(news, 5)
+        self.result["text"] = str(keywords)
 
 # 1. 元件(父元件) 2. 元件.排版(pack grid)
 window = Tk()
 window.geometry("500x500+200+200")
 
 f1 = MyFrame(window)
-f1.pack()
+f1.pack(padx=20, pady=20)
 
 window.mainloop()
