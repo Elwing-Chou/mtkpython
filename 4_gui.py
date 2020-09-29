d34f7e6b84377d054cbde5b81f21ace76b1bf814
Elwing-Chou <birdfan8814@gmail.com>
2020-09-29 13:39:15 +0800

    Add files via upload

---
+++ 4_gui.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+import jieba.analyse
+import time
+import threading
+
+class MyFrame(tk.LabelFrame):
+
+    def __init__(self, parent):
+        tk.LabelFrame.__init__(self, parent, text="分析")
+        self.pack(expand=True, fill=tk.BOTH, padx=20, pady=20)
+
+        self.e1 = tk.Text(self, height=10)
+        self.e1.pack(expand=True, fill=tk.BOTH, padx=20, pady=20)
+
+        self.b1 = tk.Button(self, text="請點我", command=self.analyze)
+        self.b1.pack(expand=True, fill=tk.BOTH, padx=20, pady=20)
+
+        self.l1 = tk.Label(self, text="分析結果!!!")
+        self.l1.pack(expand=True, fill=tk.BOTH, padx=20, pady=20)
+
+    def analyze(self):
+        def work():
+            time.sleep(5)
+            news = self.e1.get("1.0", "end")
+            keywords = jieba.analyse.extract_tags(news)
+            self.l1["text"] = str(keywords)
+        thread = threading.Thread(target=work)
+        thread.start()
+
+window = tk.Tk()
+window.geometry("500x500+200+200")
+
+# Step1. 創造元件(老爸) Step2. 排版(grid[表格], pack[上下左右])
+# 元件在創立前, 先創立tk.mainloop()Frame
+MyFrame(window)
+
+tk.mainloop()
