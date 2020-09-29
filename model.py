f7888c79180f9f8f54c21d3666da61f07f3fb24f
Elwing-Chou <birdfan8814@gmail.com>
2020-09-29 16:05:24 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -9,6 +10,7 @@ class Person(Base):
     __tablename__ = "people"
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
+    phones = relationship("Phone", backref="person")
 
     def __str__(self):
         return "{}:{}".format(self.id, self.name)
@@ -28,3 +30,28 @@ class Phone(Base):
 
     def __repr__(self):
         return str(self)
+
+if __name__ == "__main__":
+    import os
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    nowdir = os.path.dirname(__file__)
+    sqlpath = "sqlite:///" + os.path.join(nowdir, "data.sqlite")
+
+    engine = create_engine(sqlpath, echo=True)
+    Session = sessionmaker(bind=engine)
+    sess = Session()
+
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+    p1 = Person(name="Elwing")
+    sess.add(p1)
+    p1.phones = [Phone(number="0912345678"),
+                 Phone(number="0934671213")]
+    sess.commit()
+    result = sess.query(Phone).filter_by(number="0912345678")
+    p = result.first()
+    print("!", p)
+    print("!!", p.person)
+    print("!!!", p.person.phones)
