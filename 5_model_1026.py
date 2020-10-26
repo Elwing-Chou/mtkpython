7d1bc5c87fa2d50cef44a06494a1dc622d2a3ce0
Elwing-Chou <birdfan8814@gmail.com>
2020-10-26 15:50:22 +0800

    Add files via upload

--- 5_model_1026.py
+++ 5_model_1026.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -9,9 +9,23 @@ class Student(Base):
     __tablename__ = "students"
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
+    phones = relationship("Phone", backref="student")
 
     def __str__(self):
         return "{}: {}".format(self.id, self.name)
+
+    def __repr__(self):
+        return self.__str__()
+
+class Phone(Base):
+
+    __tablename__ = "phones"
+    id = Column(Integer, primary_key=True)
+    number = Column(String(32))
+    student_id = Column(Integer, ForeignKey("students.id"))
+
+    def __str__(self):
+        return "{}: {}".format(self.id, self.number)
 
     def __repr__(self):
         return self.__str__()
