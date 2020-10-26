92ae29ed636ad151b0e0676d47a23dd7f26c2617
Elwing-Chou <birdfan8814@gmail.com>
2020-10-26 13:19:44 +0800

    Add files via upload

--- 2_tk_1026.py
+++ 2_tk_1026.py
@@ -1,4 +1,11 @@
 import tkinter as tk
+import jieba.analyse
+
+def analyse():
+    global t1, l1
+    news = t1.get("1.0", "end")
+    keys = jieba.analyse.extract_tags(news, 5)
+    l1["text"] = str(keys)
 
 window = tk.Tk()
 window.geometry("500x500+300+300")
@@ -7,7 +14,7 @@ f1 = tk.Frame(window)
 f1.pack(expand=True, fill=tk.BOTH, padx=20, pady=20)
 t1 = tk.Text(f1)
 t1.pack(expand=True, fill=tk.BOTH)
-b1 = tk.Button(f1, text="請按我")
+b1 = tk.Button(f1, text="請按我", command=analyse)
 b1.pack(expand=True, fill=tk.BOTH)
 l1 = tk.Label(f1, text="按上面顯示結果")
 l1.pack(expand=True, fill=tk.BOTH)
