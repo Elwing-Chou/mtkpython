079b513dfc0f4021863493003601f1f492f4a528
Elwing-Chou <birdfan8814@gmail.com>
2021-12-13 11:15:31 +0800

    Add files via upload

--- 1_oo_1213.py
+++ 1_oo_1213.py
@@ -18,10 +18,23 @@ class Person:
 
 p1 = Person("Elwing", 175, 75)
 print(p1.bmi())
+print(Person.bmi(p1))
 # str(p1) -> p1.__str__()
 print(p1)
-# repr(p1) -> p1.__str__()
+# repr(p1) -> p1.__repr__()
 print([p1])
 p2 = Person("Bob", 180, 80)
 # p1.__eq__(p2)
 print(p1 == p2)
+
+class SuperPerson(Person):
+    def __init__(self, name, height, weight, city):
+        Person.__init__(self, name, height, weight)
+        self.city = city
+
+    def __str__(self):
+        return "{}/{}".format(Person.__str__(self), self.city)
+
+s1 = SuperPerson("Elwing", 180, 80, "Taipei")
+print(s1)
+print([s1])
