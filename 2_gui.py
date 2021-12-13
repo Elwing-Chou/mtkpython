0bd9011af348bf76d005d5dddcf9d1dd472cf1b2
Elwing-Chou <birdfan8814@gmail.com>
2021-12-13 11:42:44 +0800

    Add files via upload

--- 2_gui.py
+++ 2_gui.py
@@ -1,27 +1,18 @@
 import tkinter as tk
-import jieba.analyse
 
-def analyze():
-    global e1, l1
-    news = e1.get("1.0", "end")
-    keywords = jieba.analyse.extract_tags(news)
-    l1["text"] = str(keywords)
-
+# 變數名稱 = 創造元件(父親)
+# 變數.排版 (pack/grid/...)
 window = tk.Tk()
-window.geometry("500x500+200+200")
-
-# Step1. 創造元件(老爸) Step2. 排版(grid[表格], pack[上下左右])
-# 元件在創立前, 先創立tk.mainloop()Frame
-f1 = tk.Frame(window)
-f1.pack(expand=True, fill=tk.BOTH, padx=20, pady=20)
-
-e1 = tk.Text(f1, height=10)
-e1.pack(expand=True, fill=tk.BOTH)
-
-b1 = tk.Button(f1, text="請點我", command=analyze)
-b1.pack(expand=True, fill=tk.BOTH)
-
-l1 = tk.Label(f1, text="分析結果!!!")
-l1.pack(expand=True, fill=tk.BOTH)
-
-tk.mainloop()
+window.geometry("500x500+250+250")
+f1 = tk.Frame(window, padx=20, pady=20)
+f1.pack(expand=True, fill=tk.BOTH)
+l1 = tk.Label(f1, text="輸入文章")
+l1.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
+# single:Entry multi:Text
+t1 = tk.Text(f1)
+t1.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
+b1 = tk.Button(f1, text="分析")
+b1.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
+result = tk.Label(f1, text="結果")
+result.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
+window.mainloop()
