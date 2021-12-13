02e44bce617de692679356efccfe85c2d6c6994a
Elwing-Chou <birdfan8814@gmail.com>
2021-12-13 16:43:02 +0800

    Add files via upload

--- 6_database_1213.py
+++ 6_database_1213.py
@@ -2,27 +2,38 @@ import os
 import model_1213 as model
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import tkinter as tk
+
+class MyFrame(tk.Frame):
+
+    def __init__(self, parent, session):
+        tk.Frame.__init__(self, parent)
+        self.e1 = tk.Entry(self)
+        self.e1.pack()
+        self.b1 = tk.Button(self, text="查詢", command=self.query)
+        self.b1.pack()
+        self.result = tk.Label(self, text="電話")
+        self.result.pack()
+        self.session = session
+
+    def query(self):
+        name = self.e1.get()
+        print(name)
+        student = self.session.query(model.Student).filter_by(name=name).first()
+        print(student)
+
 
 # create_engine("sqlite:///")
 dn = os.path.dirname(__file__)
-fn =os.path.join(dn, "data.sqlite")
+fn = os.path.join(dn, "data.sqlite")
 sqlpath = "sqlite:///{}".format(fn)
 # echo=True
 engine = create_engine(sqlpath)
-model.Base.metadata.drop_all(engine)
-model.Base.metadata.create_all(engine)
-
 Session = sessionmaker(bind=engine)
 session1 = Session()
 
-p1 = model.Mobile(number="0912345678")
-p2 = model.Mobile(number="0923123421")
-s1 = model.Student(name="Elwing", phones=[p1, p2])
-session1.add(s1)
-session1.commit()
-
-# 1: first()
-result = session1.query(model.Student).filter_by(name="Elwing")
-for s in result:
-    print(s)
-    print(s.phones)
+# gui
+window = tk.Tk()
+f1 = MyFrame(window, session1)
+f1.pack()
+window.mainloop()
