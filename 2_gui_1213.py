0f59386b7dc99c8d632f042f9a48c0bbce9cd6c3
Elwing-Chou <birdfan8814@gmail.com>
2021-12-13 13:23:43 +0800

    Add files via upload

--- 2_gui_1213.py
+++ 2_gui_1213.py
@@ -1,4 +1,11 @@
 import tkinter as tk
+import jieba.analyse
+
+def analyse():
+    global t1, result
+    t = t1.get("1.0", "end")
+    tags = jieba.analyse.extract_tags(t, topK=5)
+    result["text"] = str(tags)
 
 # 變數名稱 = 創造元件(父親)
 # 變數.排版 (pack/grid/...)
@@ -11,7 +18,7 @@ l1.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
 # single:Entry multi:Text
 t1 = tk.Text(f1, height=10)
 t1.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
-b1 = tk.Button(f1, text="分析")
+b1 = tk.Button(f1, text="分析", command=analyse)
 b1.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
 result = tk.Label(f1, text="結果")
 result.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
