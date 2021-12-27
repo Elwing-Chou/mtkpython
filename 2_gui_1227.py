422b06e48a3adf7f26786c6fbdae77017366d670
Elwing-Chou <birdfan8814@gmail.com>
2021-12-27 13:19:34 +0800

    Add files via upload

--- 2_gui_1227.py
+++ 2_gui_1227.py
@@ -1,4 +1,11 @@
 import tkinter as tk
+import jieba.analyse
+
+def analyse():
+    global t1, result
+    s = t1.get("1.0", "end")
+    keywords = jieba.analyse.extract_tags(s)
+    result["text"] = str(keywords)
 
 window = tk.Tk()
 window.geometry("500x500+200+200")
@@ -13,7 +20,7 @@ l1.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
 # Entry:single line Text: multi line
 t1 = tk.Text(f1, height=10)
 t1.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
-b1 = tk.Button(f1, text="分析")
+b1 = tk.Button(f1, text="分析", command=analyse)
 b1.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
 result = tk.Label(f1, text="點擊按鈕分析")
 result.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
