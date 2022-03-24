bd27001bcdba0d9b7b8bfd7ae4598b5837a6752b
Elwing-Chou <birdfan8814@gmail.com>
2022-03-24 15:01:01 +0800

    Add files via upload

---
+++ 6_subprocess_0324.py
@@ -0,0 +1,18 @@
+import subprocess
+
+# run: shell->要不要從shell執行這個指令
+# win: dir就一定要shell=True
+# linux/mac: ls -al shell=False
+# 萬不得已不要shell=True
+# shell=True, command="ls -al&rm -rf"
+# shell=False, command=["命令", "參數"]
+# ["ls", "-al", "rm -rf"
+# C:\\
+command = [r"/usr/local/bin/python3.9",
+           "-m",
+           "pip",
+           "install",
+           "--trusted-host=pypi.org --trusted-host=files.pythonhosted.org --trusted-host=pypi.python.org",
+           "sqlalchemy==1.3.0"]
+result = subprocess.run(command)
+print(result)
