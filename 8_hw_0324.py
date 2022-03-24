d8b1efe9a9e881f8d38b64c7e6ce5ac80b1c8fbd
Elwing-Chou <birdfan8814@gmail.com>
2022-03-24 16:46:46 +0800

    Add files via upload

---
+++ 8_hw_0324.py
@@ -0,0 +1,31 @@
+from model import Student, Phone
+import tkinter as tk
+
+class MyFrame(tk.Frame):
+
+    def __init__(self, parent, session):
+        tk.Frame.__init__(self, parent)
+        self.pack()
+        self.e1 = tk.Entry(self)
+        self.e1.pack()
+        self.b1 = tk.Button(self, text="按我查詢", command=self.query)
+        self.b1.pack()
+        self.l1 = tk.Label(self, text="點擊上方")
+        self.l1.pack()
+        self.session = session
+
+    def query(self):
+        name = self.e1.get()
+        print(name)
+
+if __name__ == "__main__":
+    import os
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    dir = os.path.dirname(__file__)
+    fn = "sqlite:///" + os.path.join(dir, "data.sqlite")
+    engine = create_engine(fn, echo=True)
+    session = sessionmaker(bind=engine)()
+    window = tk.Tk()
+    MyFrame(window, session)
+    window.mainloop()
