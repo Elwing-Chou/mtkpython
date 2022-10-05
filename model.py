1a7806cf673bb89e108bce9b12df86e964b9dbcf
Elwing-Chou <birdfan8814@gmail.com>
2022-10-05 16:41:44 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -1,57 +1,59 @@
+
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-class Person(Base):
+class Student(Base):
 
-    __tablename__ = "people"
+    __tablename__ = "students"
     id = Column(Integer, primary_key=True)
-    name = Column(String(128))
-    phones = relationship("Phone", backref="person")
+    name = Column(String(64))
+    phones = relationship("Phone", backref="student")
 
     def __str__(self):
         return "{}:{}".format(self.id, self.name)
 
     def __repr__(self):
-        return str(self)
+        return self.__str__()
 
 class Phone(Base):
 
     __tablename__ = "phones"
     id = Column(Integer, primary_key=True)
     number = Column(String(32))
-    person_id = Column(Integer, ForeignKey("people.id"))
+    student_id = Column(Integer, ForeignKey("students.id"))
 
     def __str__(self):
         return "{}:{}".format(self.id, self.number)
 
     def __repr__(self):
-        return str(self)
+        return self.__str__()
 
 if __name__ == "__main__":
     import os
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
-    nowdir = os.path.dirname(__file__)
-    sqlpath = "sqlite:///" + os.path.join(nowdir, "data.sqlite")
-
-    engine = create_engine(sqlpath, echo=True)
-    Session = sessionmaker(bind=engine)
-    sess = Session()
-
+    dn = os.path.dirname(__file__)
+    fn = "sqlite:///" + os.path.join(dn, "data.sqlite")
+    # echo=True
+    engine = create_engine(fn, echo=True)
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
-    p1 = Person(name="Elwing")
-    sess.add(p1)
-    p1.phones = [Phone(number="0912345678"),
-                 Phone(number="0934671213")]
-    sess.commit()
-    result = sess.query(Phone).filter_by(number="0912345678")
-    p = result.first()
-    print("!", p)
-    print("!!", p.person)
-    print("!!!", p.person.phones)
+    Session = sessionmaker(bind=engine)
+    session1 = Session()
+
+    p1 = Phone(number="0912345678")
+    p2 = Phone(number="0932123456")
+    s1 = Student(name="Elwing", phones=[p1, p2])
+
+    session1.add(s1)
+    session1.commit()
+
+    result = session1.query(Student).filter_by(name="Elwing").first()
+    # .first()/.all() -> for in
+    print(result.phones)
+    for p in result.phones:
+        print("!", p.student)
---
+++ ormgui_1005.py
@@ -0,0 +1,38 @@
+
+
+from tkinter import *
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model import Student, Phone
+import os
+
+
+class MyFrame(Frame):
+    def __init__(self, master, engine):
+        Frame.__init__(self, master)
+        self.l1 = Label(text="輸入姓名")
+        self.l1.pack()
+        self.e1 = Entry()
+        self.e1.pack()
+        self.b1 = Button(text="查詢", command=self.query_phone)
+        self.b1.pack()
+        self.result = Label(text="按上面查詢")
+        self.result.pack()
+        S = sessionmaker(bind=engine)
+        self.session = S()
+
+    def query_phone(self):
+        n = self.e1.get()
+
+        self.result["text"] = str(result.phones)
+
+
+window = Tk()
+window.geometry("500x500")
+dn = os.path.dirname(__file__)
+fn = "sqlite:///" + os.path.join(dn, "data.sqlite")
+engine = create_engine(fn, echo=False)
+
+f1 = MyFrame(window, engine)
+f1.pack()
+window.mainloop()
