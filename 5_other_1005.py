d2e953a349bf22f78f41b08393ac7f43566e9519
Elwing-Chou <birdfan8814@gmail.com>
2022-10-05 15:08:31 +0800

    Add files via upload

--- 5_other_1005.py
+++ 5_other_1005.py
@@ -7,3 +7,10 @@ print(result)
 # command = "dir"
 # result = subprocess.run(command, shell=True)
 # print(result)
+
+command = ["/usr/local/bin/python3.9",
+           "test.py"]
+
+with open("out.txt", "w") as f1, \
+     open("err.txt", "w") as f2:
+    subprocess.run(command, stdout=f1, stderr=f2)
