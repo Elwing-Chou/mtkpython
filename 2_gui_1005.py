9b00d5d57b1e6f3704e8be7e13df9e92753a18d1
Elwing-Chou <birdfan8814@gmail.com>
2022-10-05 13:20:45 +0800

    Add files via upload

--- 2_gui_1005.py
+++ 2_gui_1005.py
@@ -8,9 +8,18 @@ window.geometry("500x500+200+200")
 # 2. 排版(pack[左右上下]/grid[表格]/absolute[絕對])
 # 建議pack
 f1 = tk.Frame(window)
-f1.pack()
+f1.pack(expand=True, fill=tk.BOTH, padx=20, pady=20)
 
 l1 = tk.Label(f1, text="輸入文章:")
-l1.pack()
+l1.pack(expand=True, fill=tk.BOTH)
+
+t1 = tk.Text(f1)
+t1.pack(expand=True, fill=tk.BOTH)
+
+b1 = tk.Button(f1, text="關鍵詞分析")
+b1.pack(expand=True, fill=tk.BOTH, pady=10)
+
+result = tk.Label(f1, text="點擊上面分析")
+result.pack(expand=True, fill=tk.BOTH)
 
 window.mainloop()
